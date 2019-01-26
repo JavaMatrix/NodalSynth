ff5af0457fb4c373005832fd46cf2fd1602fc185
Nathanael Page <nathanael.o.page@gmail.com>
2019-01-25 22:12:36 -0500

    Hooking up SFML

--- NodalSynth/Main.cpp
+++ NodalSynth/Main.cpp
@@ -1,9 +1,24 @@
-#include <stdio.h>
-#include <conio.h>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	puts("Hello world");
-	printf("Press any key to continue...");
-	_getch();
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
